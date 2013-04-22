ec94abd5f93145ebb13a84f073888070928ceca7
Chris Woodford <chris.woodford@gmail.com>
2013-04-22 12:04:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Able Client Hosting
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbleHosting::Application.config.secret_token = '858c95a9766e97d450bbbc480877e9ef643b363296df1a2649d31217493b5e6a8d01f118eeefdb8adefc4ea8bef0cd889631032588cb7f5b1ecea64ffcb6b6c7'
