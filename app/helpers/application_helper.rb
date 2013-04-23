91f59117a7247ad5d85ec7b02f377ef87200bee2
Chris Woodford <chris.woodford@gmail.com>
2013-04-23 15:38:45 -0400

    initialize stripe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
   def viewport_tag
-    tag 'meta', name: 'viewport', content: 'width=device-width, initial-scale=1.0'
+    tag "meta", name: "viewport", content: "width=device-width, initial-scale=1.0"
+  end
+
+  def stripe_javascript_tag(version = "v2")
+    javascript_include_tag "https://js.stripe.com/#{version}/"
+  end
+
+  def stripe_meta_tag
+    tag "meta", name: "stripe-publishable-token", content: "#{STRIPE_PUBLISHABLE_KEY}"
   end
 
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,9 @@
-Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-STRIPE_PUBLISHABLE_KEY = ENV['STRIPE_PUBLISHABLE_KEY']
+Rails.application.config.stripe = {
+  publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY']
+}
+
+Stripe.api_key = Rails.application.config.stripe[:secret_key]
+Stripe.api_version = "2013-02-13"
+
+STRIPE_PUBLISHABLE_KEY = Rails.application.config.stripe[:publishable_key]
