e0f3a0e0cb014f4e765a240de8a25d5ac7e12b75
Chris Woodford <chris.woodford@gmail.com>
2013-04-23 17:22:07 -0400

    create the subscrition creation form

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,6 +2,20 @@ class SubscriptionsController < ApplicationController
 
   def new
 
+    unless params[:plan_id]
+      return redirect_to plans_path
+    end
+
+    @plan = Plan.find params[:plan_id]
+
+  end
+
+  def create
+
+    @plan = Plan.find params[:plan_id]
+
+    render "new"
+
   end
 
 end
